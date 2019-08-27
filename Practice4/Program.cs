d26791eee6d9de5cdf818e5bb5f8178cb041499d
Jahvier Hernandez <jahvierhernandez11@gmail.com>
2019-08-26 18:00:59 -0700

    first commit

---
+++ Practice4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice4
+{
+    public class Animal
+    {
+        public int NumberofLegs = 6 + 2 / 2;
+        public int NumberofEyes = 2 * 1;
+        public int NumberofArms = 1 + 1;
+        public string says;
+        public void Speak()
+        {
+            Console.WriteLine(says);
+        }
+    }
+
+    public class Dog
+    {
+        for(int NumberofLegs = 6 + 2 / 2;  )
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
